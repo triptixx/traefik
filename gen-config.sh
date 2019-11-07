30524d7944db0e5f6f8a7ce2b137ba4af734892c
Triptixx <54292597+triptixx@users.noreply.github.com>
2019-11-07 09:59:59 +0100

    Update gen-config.sh

--- gen-config.sh
+++ gen-config.sh
@@ -105,7 +105,6 @@ http:
         scheme: https
     defaultHeader:
       headers:
-        sslRedirect: true
         stsSeconds: 63072000
         stsIncludeSubdomains: true
         stsPreload: true
@@ -121,14 +120,12 @@ tls:
       sniStrict: true
       minVersion: VersionTLS13
       cipherSuites:
-        - TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305
-        - TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305
-        - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
         - TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
-        - TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
+        - TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
         - TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
-        - TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256
-        - TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256
+        - TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
+        - TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305
+        - TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305
 EOL
 
 fi
